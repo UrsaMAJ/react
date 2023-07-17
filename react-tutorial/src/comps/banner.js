35407c6b2740348e00be08ae88edea014f7249d1
Austin Demott <90654141+UrsaMAJ@users.noreply.github.com>
2023-07-17 15:38:23 -0400

    first component

---
+++ react-tutorial/src/comps/banner.js
@@ -0,0 +1,11 @@
+
+import './banner.css'
+function HelloWolrd(){
+    return(
+        <div >
+            <p className='banner'> Hello World</p>
+        </div>
+    )
+}
+
+export default HelloWolrd;
